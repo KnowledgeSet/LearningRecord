e79aac8345daef6ef1007289205e9ef64c406957
BY_R <468296943@qq.com>
2018-02-06 17:15:34 +0800

    add storyboard

--- LearningRecord/LearningRecord/tests/data_struct/linked_list/BYLinkedList.h
+++ LearningRecord/LearningRecord/tests/data_struct/linked_list/BYLinkedList.h
@@ -4,17 +4,41 @@
 //
 //  Created by by_r on 2018/2/6.
 //  Copyright © 2018年 by_r. All rights reserved.
-//
+//  双向链表抽象数据类型
 
 #import <Foundation/Foundation.h>
-
+/// 节点
 @interface BYLinkedListNode : NSObject
-@property (nonatomic, weak) id previous;
-@property (nonatomic, strong) id next;
-@property (nonatomic, strong) id value;
+@property (nonatomic, weak) BYLinkedListNode *previous;    ///< 直接前驱
+@property (nonatomic, strong) BYLinkedListNode *next;      ///< 直接后继
+@property (nonatomic, strong) id value;                 ///< 数据元素
 - (instancetype)initWithValue:(id)value;
 @end
 
 @interface BYLinkedList : NSObject
+@property (nonatomic, assign, getter=isEmpty, readonly) BOOL    empty;  ///< 是否为空链表
+@property (nonatomic, assign, readonly) NSInteger   count;              ///< 链表中的元素数量
+@property (nonatomic, strong, readonly) BYLinkedListNode    *first;       ///< 第一个元素
+@property (nonatomic, strong, readonly) BYLinkedListNode    *last;        ///< 最后一个元素
 
+/// 获取指定位置的元素
+- (BYLinkedListNode *)nodeAtIndex:(NSInteger)index;
+/// 在尾部插入一个元素
+- (void)appendToTail:(id)value;
+/// 在头部插入一个元素
+- (void)insertToHead:(id)value;
+/// 在指定位置插入一个节点
+- (void)insert:(BYLinkedListNode *)node atIndex:(NSInteger)index;
+
+/// 移除所有节点
+- (void)removeAll;
+/// 移除最后一个节点
+- (id)removeLast;
+/// 移除一个节点
+- (id)remove:(BYLinkedListNode *)node;
+/// 移除指定位置的节点
+- (id)removeAtIndex:(NSInteger)index;
+
+/// 打印所有节点
+- (void)printAllNodes;
 @end
--- LearningRecord/LearningRecord/tests/data_struct/linked_list/BYLinkedList.m
+++ LearningRecord/LearningRecord/tests/data_struct/linked_list/BYLinkedList.m
@@ -18,6 +18,173 @@
 }
 @end
 
-@implementation BYLinkedList
+@interface BYLinkedList ()
+@property (nonatomic, strong) BYLinkedListNode  *head;
+@end
 
+@implementation BYLinkedList
+- (BOOL)isEmpty {
+    return _head == nil;
+}
+
+- (NSInteger)count {
+    if (_head == nil) {
+        return 0;
+    }
+    NSInteger count = 1;
+    BYLinkedListNode *node = _head.next;
+    while (node) {
+        node = node.next;
+        count ++;
+    }
+    return count;
+}
+
+- (BYLinkedListNode *)first {
+    return _head;
+}
+
+- (BYLinkedListNode *)last {
+    if (_head == nil) {
+        return nil;
+    }
+    BYLinkedListNode *node = _head;
+    while (node.next) {
+        node = node.next;
+    }
+    return node;
+}
+
+- (BYLinkedListNode *)nodeAtIndex:(NSInteger)index {
+    if (index == 0) {
+        return _head;
+    }
+    if (index >= self.count) {
+        return nil;
+    }
+    BYLinkedListNode *node = _head.next;
+    for (NSInteger i = 1; i < index; i ++) {
+        if (node == nil) {
+            break;
+        }
+        node = node.next;
+    }
+    return node;
+}
+
+- (void)appendToTail:(id)value {
+    BYLinkedListNode *newNode = [[BYLinkedListNode alloc] initWithValue:value];
+    if (self.last == nil) {
+        _head = newNode;
+    }
+    else {
+        newNode.previous = self.last;
+        self.last.next = newNode;
+    }
+}
+
+- (void)insertToHead:(id)value {
+    BYLinkedListNode *newHead = [[BYLinkedListNode alloc] initWithValue:value];
+    if (_head == nil) {
+        _head = newHead;
+    }
+    else {
+        newHead.next = _head;
+        _head.previous = newHead;
+        _head = newHead;
+    }
+}
+
+- (void)insert:(BYLinkedListNode *)node atIndex:(NSInteger)index {
+    if (index < 0) {
+        NSLog(@"index out of range");
+        return;
+    }
+    
+    if (self.count == 0) {
+        _head = node;
+    }
+    else {
+        if (index == 0) {
+            node.next = _head;
+            _head.previous = node;
+            _head = node;
+        }
+        else {
+            // 当index == self.count时，实际上是插入尾部，不算是越界，只是下面的next为空
+            // 不在开始时判断是因为如果链表为空，则添加在头部
+            if (index > self.count) {
+                NSLog(@"index out of range");
+                return;
+            }
+            BYLinkedListNode *prev = [self nodeAtIndex:index - 1];
+            BYLinkedListNode *next = prev.next;
+            
+            node.previous = prev;
+            node.next = next;
+            prev.next = node;
+            next.previous = node;
+        }
+    }
+}
+
+- (void)removeAll {
+    // 此处描述的非循环链表，只需将头部置空即可
+    _head = nil;
+}
+
+- (id)removeLast {
+    if (self.isEmpty) {
+        return nil;
+    }
+    return [self remove:self.last];
+}
+
+- (id)remove:(BYLinkedListNode *)node {
+    if (_head == nil) {
+        NSLog(@"linked list is empty");
+        return nil;
+    }
+    BYLinkedListNode *prev = node.previous;
+    BYLinkedListNode *next = node.next;
+    if (prev) {
+        prev.next = next;
+    }
+    else {
+        _head = next;
+    }
+    next.previous = prev;
+    
+    node.previous = nil;
+    node.next = nil;
+    return node.value;
+}
+
+- (id)removeAtIndex:(NSInteger)index {
+    if (_head == nil) {
+        NSLog(@"linked list is empty");
+        return nil;
+    }
+    BYLinkedListNode *node = [self nodeAtIndex:index];
+    if (!node) {
+        return nil;
+    }
+    return [self remove:node];
+}
+
+- (void)printAllNodes {
+    if (_head == nil) {
+        NSLog(@"linked list is empty");
+        return;
+    }
+    BYLinkedListNode *node = _head;
+    NSLog(@"\nstart printing all nodes:");
+    for (NSInteger i = 0; i < self.count; i ++) {
+        if (node == nil) {
+            break;
+        }
+        printf("[%@]%@->%@",@(i),node.value,node);
+        node = node.next;
+    }
+}
 @end
